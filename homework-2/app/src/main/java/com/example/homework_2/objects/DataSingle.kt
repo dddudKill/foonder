bfb59fa73b3a2056768ebc7a4691b1370a8187a8
dddudKill <100380974+dddudKill@users.noreply.github.com>
2022-12-26 17:41:28 +0300

    Delete homework-2 directory

--- homework-2/app/src/main/java/com/example/homework_2/objects/DataSingle.kt
+++
@@ -1,7 +0,0 @@
-package com.example.homework_2.objects
-
-import com.google.gson.annotations.SerializedName
-
-class DataSingle {
-    @SerializedName("data") var gif: Gif = Gif()
-}
--- homework-2/app/src/main/java/com/example/homework_2/objects/Pagination.kt
+++
@@ -1,9 +0,0 @@
-package com.example.homework_2.objects
-
-import com.google.gson.annotations.SerializedName
-
-class Pagination {
-    var offset = 0
-    @SerializedName("total_count") var totalCount = 0
-    var count = 0
-}
